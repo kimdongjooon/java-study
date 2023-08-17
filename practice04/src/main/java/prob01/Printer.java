ea815e3763f996c314251fd426175d2f69365098
kimdongjooon <ehdwns6745@naver.com>
2023-08-17 09:33:03 +0900

    오버로딩, 제너릭, 오브젝트로 각각 구현

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,19 +2,30 @@ package prob01;
 
 public class Printer {
 	
-	public void println(int i) {
-		System.out.println(i);
+	// 일반적인 오버로딩.
+//	public void println(int i) {
+//		System.out.println(i);
+//	}
+//	
+//	public void println(boolean b) {
+//		System.out.println(b);
+//	}
+//	
+//	public void println(double d) {
+//		System.out.println(d);
+//	}
+//	
+//	public void println(String str) {
+//		System.out.println(str);
+//	}
+	
+	// 제너릭으로 사용하기 
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
 	
-	public void println(boolean b) {
-		System.out.println(b);
-	}
-	
-	public void println(double d) {
-		System.out.println(d);
-	}
-	
-	public void println(String str) {
-		System.out.println(str);
-	}
+	// 오브젝트로 사용하기  
+//	public void println(Object o) {
+//		System.out.println(o);
+//	}
 }
